a24c1cea7a82b7432f4a9f296ddac7b62bc586d0
Erlin Zhao <158342004+zel701@users.noreply.github.com>
2026-09-24 08:12:57 -0700

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        maxL = 0
+        for i in wordDict:
+            maxL = max(maxL,len(i))
+        wordDict = set(wordDict)
+        dp = [False] * (len(s)+1)
+        dp[0] = True
+        for i in range(len(dp)):
+            for j in range(i,-1,-1):
+                print(s[j:i])
+                if s[j:i] in wordDict:
+                    if dp[j]:
+                        dp[i] = True
+                        break
+                if i-j > maxL:
+                    break
+        return dp[-1]
